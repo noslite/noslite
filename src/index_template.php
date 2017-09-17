3497f358af8fd5026e5542978a4ce4b8047340b0
Harmen <harmenstoppels@gmail.com>
2017-09-17 12:44:42 +0200

    Minify CSS

--- src/index_template.php
+++ src/index_template.php
@@ -7,43 +7,7 @@ function render_index(string $header, string $content) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>NOS lite</title>
-  <style>
-    html,body {
-      font-size: 62.5%;
-      padding: 0;
-      margin: 0;
-    }
-    body {
-      font-size: 1.6em;
-      line-height: 1.5;
-      font-family: Helvetica,sans-serif;
-    }
-    p, ul {
-      font-size: 1em;
-      line-height: 1.5;
-      margin: 1.5em 0;
-    }
-    h2 {
-      font-size:  1.125em; /* equiv 18px */
-      line-height:  1.3333;
-      margin:  1.3333em 0 0 0;
-    }
-    header {
-      padding: 10px 20px;
-      border-bottom: 1px solid #ccc;
-      color: #AAA;
-    }
-    main {
-      padding: 20px;
-    }
-    ul {
-      list-style-position: inside;
-      padding-left: 0;
-    }
-    li {
-      padding-bottom: 10px;
-    }
-  </style>
+  <style>html,body{font-size:62.5%;padding:0;margin:0}body{font-size:1.6em;line-height:1.5;font-family:Helvetica,sans-serif}p,ul{font-size:1em;line-height:1.5;margin:1.5em 0}h2{font-size:1.125em;line-height:1.3333;margin:1.3333em 0 0 0}header{padding:10px 20px;border-bottom:1px solid #ccc;color:#AAA}main{padding:20px}ul{list-style-position:inside;padding-left:0}li{padding-bottom:10px}</style>
 </head>
 <body>
   <header>$header</header>
