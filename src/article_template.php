e6ddec8d6a1b6dc152c7fd4dbbb81736912f90b6
Harmen <harmenstoppels@gmail.com>
2017-09-17 21:59:29 +0200

    Header -> footer

--- src/article_template.php
+++ src/article_template.php
@@ -7,13 +7,11 @@ function render_article(string $title, string $content) : string {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>$title - NOS lite</title>
-  <style>html,body{font-size:62.5%;padding:0;margin:0}body{font-size:1.6em;line-height:1.5;font-family:Helvetica,sans-serif}p,ul{font-size:1em;line-height:1.5;margin:1.5em 0}h2{font-size:1.125em;line-height:1.3333;margin:1.3333em 0 0 0}header{padding:10px 20px;border-bottom:1px solid #ccc;color:#AAA}main{padding:20px;max-width:600px}ul{margin:10px 0}li{padding-bottom:10px}</style>
+  <style>html,body{font-size:62.5%;padding:0;margin:0;display:flex;flex-direction:column}body{font-size:1.6em;line-height:1.5;font-family:Helvetica,sans-serif}p,ul{font-size:1em;line-height:1.5;margin:1.5em 0}p:first-child{margin-top:0}h2{font-size:1.125em;line-height:1.3333;margin:1.3333em 0 0 0}footer{padding:10px 20px;border-top:1px solid #ccc;color:#AAA;max-width:600px;display:flex;justify-content:space-between;align-items:center}main{padding:20px;max-width:600px}ul{margin:10px 0}li{padding-bottom:10px}.b{margin-left:1em}</style>
 </head>
 <body>
-  <header><a href="/">Terug</a> | $title</header>
-  <main>
-    $content
-  </main>
+  <main>$content</main>
+  <footer>$title <a href="/" class="b">Terug</a></footer>
 </body>
 </html>
 EOT;
