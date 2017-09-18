b7f0f92b6729cba1f7ca7fe99854f6c0ae58abdd
Harmen Stoppels <harmenstoppels@gmail.com>
2017-09-18 09:45:32 +0200

    Remove list style type in index

--- src/index_template.php
+++ src/index_template.php
@@ -7,7 +7,7 @@ function render_index(string $header, string $content) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>NOS lite</title>
-  <style>html,body{font-size:62.5%;padding:0;margin:0}body{font-size:1.6em;line-height:1.5;font-family:Helvetica,sans-serif}p,ul{font-size:1em;line-height:1.5;margin:1.5em 0}h2{font-size:1.125em;line-height:1.3333;margin:1.3333em 0 0 0}footer{padding:10px 20px;border-top:1px solid #ccc;color:#AAA}main{margin:20px}ul{padding-left:0}li{padding-bottom:10px}</style>
+  <style>html,body{font-size:62.5%;padding:0;margin:0}body{font-size:1.6em;line-height:1.5;font-family:Helvetica,sans-serif}p,ul{font-size:1em;line-height:1.5;margin:1.5em 0}h2{font-size:1.125em;line-height:1.3333;margin:1.3333em 0 0 0}footer{padding:10px 20px;border-top:1px solid #ccc;color:#AAA}main{margin:20px}ul{padding-left:0;list-style-type:none}li{padding-bottom:10px}</style>
 </head>
 <body>
   <main>$content</main>
