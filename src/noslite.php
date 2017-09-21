f540c133b0b953b8c24b9518a1c0ec16817ea302
Harmen <harmenstoppels@gmail.com>
2017-09-21 22:15:16 +0200

    Use time zone

--- src/noslite.php
+++ src/noslite.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+date_default_timezone_set('Europe/Amsterdam');
+
 const URL = 'http://feeds.nos.nl/nosnieuwsalgemeen';
 
 final class Unavailable extends Exception {}
